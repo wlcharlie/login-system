529c25fa6ae273b6571303476652a5af0aa6ab4a
wlcharlie <wl00606352@gmail.com>
2021-05-09 00:29:14 +0800

    Revert "login verify with cookie"
    This reverts commit e96a5ea6591ed3da708283ad5aaa8a850f3dae91.

--- app.js
+++ app.js
@@ -1,5 +1,4 @@
 const express = require('express')
-const cookieParser = require('cookie-parser')
 const exphbs = require('express-handlebars')
 const users = require('./models/users')
 const loginVerify = require('./loginVerify')
@@ -9,16 +8,10 @@ const port = 4000
 
 app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
 app.set('view engine', 'handlebars')
-app.use(cookieParser('55688'))
 app.use(express.urlencoded({ extended: true }))
 
-
 app.get('/', (req, res) => {
-  if (Object.keys(req.signedCookies).length) {
-    res.redirect('/welcome')
-  } else {
-    res.render('index')
-  }
+  res.render('index')
 })
 
 app.post('/', (req, res) => {
@@ -27,21 +20,11 @@ app.post('/', (req, res) => {
   const loginError = loginVerify(req.body).loginError
 
   if (loginStatus) {
-    res.clearCookie('firstName')
-    res.cookie('firstName', loginUser, { signed: true, maxAge: 30000 })
-    res.redirect('/welcome')
+    res.render('welcome', { loginUser })
   } else {
     res.render('index', { loginError })
   }
-})
 
-app.get('/welcome', (req, res) => {
-  if (Object.keys(req.signedCookies).length) {
-    const loginUser = req.signedCookies.firstName
-    res.render('welcome', { loginUser })
-  } else {
-    res.redirect('/')
-  }
 })
 
 app.listen(port, () => {
